2e29a4be6a44e722d02ff4c69c8dfbb1d58c0fdd
Po Co <polniolino@gmail.com>
2025-02-15 15:48:56 +0100

    Ajout de l'effet d'apparition dans la page d'accueil

---
+++ js/page_accueil.js
@@ -0,0 +1,26 @@
+const sectionPast = document.querySelector("section#past");
+const sectionFuture = document.querySelector("section#future");
+const positions = [0, 0.1, 0.2, 0.3, 0.4];
+
+function onScroll() {
+	for (const section of [sectionPast, sectionFuture]) {
+		const sectionRect = section.getBoundingClientRect();
+		const aside = section.querySelector("aside");
+
+		const children = aside.children;
+		for (const i in positions) {
+			const position = positions[i];
+			const element = children[i];
+			if (element === undefined) {
+				break;
+			}
+			if (-sectionRect.y >= position * sectionRect.height) {
+				element.classList.remove("hidden");
+			} else {
+				element.classList.add("hidden");
+			}
+		}
+	}
+}
+document.addEventListener("scroll", onScroll);
+onScroll();
