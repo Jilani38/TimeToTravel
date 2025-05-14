8d1df99341538af5e4f0f21ecd80f2f69d0cc6d9
Po Co <polniolino@gmail.com>
2025-05-14 14:17:27 +0200

    Fix bugs visuels

--- php/partials/nav.php
+++ php/partials/nav.php
@@ -25,7 +25,7 @@
     <?php endif; ?>
 
     <button id="toggle-dark-mode">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
+      <img src="../img/dark-mode.svg">
     </button>
   </div>
 </nav>
