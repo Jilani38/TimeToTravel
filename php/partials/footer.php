14f5b7f6f57d401b21a9c0b6c976239f1e3e4b7d
Jilani38 <mohamed.jilani38@gmail.com>
2025-05-19 00:37:32 +0200

    debut footer

---
+++ php/partials/footer.php
@@ -0,0 +1,37 @@
+<!-- partials/footer.php -->
+<footer class="site-footer">
+  <div class="footer-container">
+    <div class="footer-col logo-col">
+      <h2 class="logo-footer">Time2Travel</h2>
+      <p>Explorez le passé, vivez le futur.</p>
+    </div>
+    <div class="footer-col">
+      <h3>Navigation</h3>
+      <ul>
+        <li><a href="../page_accueil.php">Accueil</a></li>
+        <li><a href="../page_recherche.php">Voyages</a></li>
+        <li><a href="../page_connexion.php">Connexion</a></li>
+        <li><a href="../page_inscription.php">Inscription</a></li>
+      </ul>
+    </div>
+    <div class="footer-col">
+      <h3>Informations</h3>
+      <ul>
+        <li><a href="../cgu.php">Conditions Générales</a></li>
+        <li><a href="../contact.php">Contact</a></li>
+        <li><a href="mailto:contact@t2t.com">contact@t2t.com</a></li>
+      </ul>
+    </div>
+    <div class="footer-col">
+      <h3>Suivez-nous</h3>
+      <div class="social-icons">
+        <a href="#"><img src="../img/icons/facebook.svg" alt="Facebook" /></a>
+        <a href="#"><img src="../img/icons/twitter.svg" alt="Twitter" /></a>
+        <a href="#"><img src="../img/icons/instagram.svg" alt="Instagram" /></a>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; <?= date('Y') ?> Time2Travel. Tous droits réservés.</p>
+  </div>
+</footer>
